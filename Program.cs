370c8fdce1e582ad6718145a3a133413e8baf43c
Piotr Szydlak <pedrosx@o2.pl>
2023-12-04 10:56:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Podaj liczbę: ");
+int a = Int32.Parse(Console.ReadLine());
+/*
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Liczba {a} jest parzysta");
+}
+else
+{
+    Console.WriteLine($"Liczba {a} nie jest parzysta"); */
+}
+if (a < 0)
+{
+    Console.WriteLine($"Liczba {a}  jest ujemna");
+}
+else
+{
+    Console.WriteLine($"Liczba {a} jest dodatnia");
+}
